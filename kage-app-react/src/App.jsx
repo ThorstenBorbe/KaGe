96407de7b6e62dcb9917381bbd8c26f6f9338f2c
ThorstenBorbe <thorsten.borbe@e3at.com>
2026-04-03 08:56:03 +0200

    Versionskennung hochgezogen

--- kage-app-react/src/App.jsx
+++ kage-app-react/src/App.jsx
@@ -16,7 +16,7 @@ import {
 } from "./utils/appNavigation";
 import { theme } from "./styles/theme";
 
-const APP_VERSION = "v0.0.8";
+const APP_VERSION = "v0.0.9";
 
 export default function App() {
   const {
